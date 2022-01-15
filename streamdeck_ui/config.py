16d8d64eddaaaf879ff3579f8eec88f93cb9fdb0
Dan Nemec <dan@nem.ec>
2022-01-14 18:21:45 -0600

    Support expanding home directory in STREAMDECK_UI_CONFIG environment variable

--- streamdeck_ui/config.py
+++ streamdeck_ui/config.py
@@ -5,5 +5,5 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 LOGO = os.path.join(PROJECT_PATH, "logo.png")
 FONTS_PATH = os.path.join(PROJECT_PATH, "fonts")
 DEFAULT_FONT = os.path.join("roboto", "Roboto-Regular.ttf")
-STATE_FILE = os.environ.get("STREAMDECK_UI_CONFIG", os.path.expanduser("~/.streamdeck_ui.json"))
+STATE_FILE = os.path.expanduser(os.environ.get("STREAMDECK_UI_CONFIG", "~/.streamdeck_ui.json"))
 CONFIG_FILE_VERSION = 1  # Update only if backward incompatible changes are made to the config file
